a14425605944e1ea002f85bd9ce2011944572ae9
quditwolf <shabdsahni2005@gmail.com>
2024-04-02 17:38:54 +0530

    some debug statements

--- web-client/main.py
+++ web-client/main.py
@@ -59,14 +59,15 @@ async def chat(event: Event):
             }
         #repair and accept response from html side
         #await websocket.send_json(response)
-        print(response)
-        await manager.broadcast(f"{sender} wrote: {event.content}")
+        print("RECEIVED",response)
+        await manager.send_personal_message(f"{sender} wrote: {event.content}")
+#        await manager.broadcast(f"{sender} wrote: {event.content}")
 
 #to get from local website and send to other user via tor
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
-    await manager.broadcast(f"Client #{client_id} joined the chat")
+#    await manager.broadcast(f"Client #{client_id} joined the chat")
     #todo tor broadcast join event
     tor.post(url,{"sender":sender, "content": "joined"})
     try:
@@ -74,12 +75,12 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
             print("while mein phasa")
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+#            await manager.broadcast(f"Client #{client_id} says: {data}")
             #todo send message via tor to all connected clients
-            tor.post(url,{"sender":sender, "content": data})
+            tor.post(url,{"sender":sender, "content":"thru tor" +  data})
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+#        await manager.broadcast(f"Client #{client_id} left the chat")
         #todo tor broadcast leave event
         tor.post(url,{"sender":sender, "content": "left"})
 
